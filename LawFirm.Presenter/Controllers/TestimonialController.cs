7b0cffc82e4eb8fb210f9d0342756cc38cf6efc3
Anastasiya Kislach <anastasiya.kislach@gmail.com>
2016-10-27 17:02:46 +0300

    блог, оформление комментов

--- LawFirm.Presenter/Controllers/TestimonialController.cs
+++ LawFirm.Presenter/Controllers/TestimonialController.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LawFirm.Presenter.Controllers
-{
-    public class TestimonialController : Controller
-    {
-        // GET: Testimonial
-        public ActionResult CreateTestimonial()
-        {
+namespace LawFirm.Presenter.Controllers {
+	public class TestimonialController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+		// GET: Testimonial
+		public ActionResult CreateTestimonial() {
 			if (ModelState.IsValid) {
 
 			}
 			return PartialView("_CreateTestimonialResult");
-        }
-    }
+		}
+	}
 }
